feddeffd931cfb8507f53f51543803215666a7c7
Sanam-Chaudhary-Code <sanamchaudhary344@gmail.com>
2025-06-19 19:03:14 +0545

    add shopify theme

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-4xl text-red-500">Gold Tree Group</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
